48504635ffbeee8b21fd2c3c4763f7f8db57e4b9
lhccong <771901874@qq.com>
2025-02-20 17:27:05 +0800

    feat:修改格式，使用在线地址图标

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,6 @@
 import {Col, Row, Card, Badge, Image, List, Typography} from 'antd';
 import React from "react";
-import zhihu from '@/assets/zhihu.png'; // 引入图片
-import douban from '@/assets/douban.png'; // 引入图片
+
 const data = [
   'Racing car sprays burning fuel into crowd.',
   'Japanese princess to wed commoner.',
@@ -13,14 +12,14 @@ export default () => {
   // @ts-ignore
   return (
     <>
-      <Row gutter={16}>
+      <Row gutter={[16,16]}>
         <Col span={12}>
           <Badge.Ribbon text="知乎">
             <Card
               title={
                 <div style={{display: 'flex', alignItems: 'center'}}>
                   <Image
-                    src={zhihu}
+                    src={"https://s1.aigei.com/src/img/png/5a/5a7a40a62b6f4383a2d3426a069b2893.png?imageMogr2/auto-orient/thumbnail/!133x132r/gravity/Center/crop/133x132/quality/85/%7CimageView2/2/w/133&e=2051020800&token=P7S2Xpzfz11vAkASLTkfHN7Fw-oOZBecqeJaxypL:J6OKFOfvHnmVEfjEvu9HYKrEhuU="}
                     preview={false} // 不启用预览
                     style={{ width: 24, height: 24, marginRight: 8 }}
                   />
@@ -40,20 +39,52 @@ export default () => {
           </Badge.Ribbon>
         </Col>
         <Col span={12}>
-          <Badge.Ribbon text="豆瓣">
+          <Badge.Ribbon text="知乎">
             <Card
               title={
                 <div style={{display: 'flex', alignItems: 'center'}}>
                   <Image
-                    src={douban}
+                    src={"https://s1.aigei.com/src/img/png/5a/5a7a40a62b6f4383a2d3426a069b2893.png?imageMogr2/auto-orient/thumbnail/!133x132r/gravity/Center/crop/133x132/quality/85/%7CimageView2/2/w/133&e=2051020800&token=P7S2Xpzfz11vAkASLTkfHN7Fw-oOZBecqeJaxypL:J6OKFOfvHnmVEfjEvu9HYKrEhuU="}
                     preview={false} // 不启用预览
                     style={{ width: 24, height: 24, marginRight: 8 }}
                   />
-                  豆瓣热话
+                  知乎热榜
                 </div>
               }
             >
-              Card content
+              <List
+                dataSource={data}
+                renderItem={(item) => (
+                  <List.Item>
+                    <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                  </List.Item>
+                )}
+              />
+            </Card>
+          </Badge.Ribbon>
+        </Col>
+        <Col span={12}>
+          <Badge.Ribbon text="知乎">
+            <Card
+              title={
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                  <Image
+                    src={"https://s1.aigei.com/src/img/png/5a/5a7a40a62b6f4383a2d3426a069b2893.png?imageMogr2/auto-orient/thumbnail/!133x132r/gravity/Center/crop/133x132/quality/85/%7CimageView2/2/w/133&e=2051020800&token=P7S2Xpzfz11vAkASLTkfHN7Fw-oOZBecqeJaxypL:J6OKFOfvHnmVEfjEvu9HYKrEhuU="}
+                    preview={false} // 不启用预览
+                    style={{ width: 24, height: 24, marginRight: 8 }}
+                  />
+                  知乎热榜
+                </div>
+              }
+            >
+              <List
+                dataSource={data}
+                renderItem={(item) => (
+                  <List.Item>
+                    <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                  </List.Item>
+                )}
+              />
             </Card>
           </Badge.Ribbon>
         </Col>
