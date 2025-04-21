57e591089f550898a9704d38dc167eb97b4dad98
lhccong <771901874@qq.com>
2025-04-21 09:09:34 +0800

    fix：移除冗余代码

--- src/global.tsx
+++ src/global.tsx
@@ -1,9 +1,5 @@
 import '@umijs/max';
-import { Button, message, notification } from 'antd';
 import defaultSettings from '../config/defaultSettings';
-const { pwa } = defaultSettings;
-const isHttps = document.location.protocol === 'https:';
-
 const clearCache = () => {
   // remove all caches
   if (window.caches) {
