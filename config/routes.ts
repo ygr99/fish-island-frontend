33aaa32ab73c79868189c4aae81b9701029e7e6d
lhccong <771901874@qq.com>
2025-04-26 10:15:49 +0800

    feat:新增共建功能

--- config/routes.ts
+++ config/routes.ts
@@ -46,6 +46,7 @@ export default [
     name: '支持网站',
     routes: [
       { icon: 'DesktopOutlined', path: '/rank/reward', component: './Rank/Reward', name: '打赏榜 👑', requireAuth: false },
+      { icon: 'DesktopOutlined', path: '/rank/about', component: './About', name: '共建与反馈 🚀', requireAuth: false },
       {path: 'https://github.com/lhccong/fish-island-backend', name: '狠狠点个 star 🌟'},
 
     ],
