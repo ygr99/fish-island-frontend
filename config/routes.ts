84ff54f91b08a6b95cd680e63351c1fc14a7f855
lhccong <771901874@qq.com>
2025-04-28 16:01:48 +0800

    Revert "fix：优化网站支持"
    This reverts commit 583cd864cf8e4f7f7298a466529b61454e51dbc4.

--- config/routes.ts
+++ config/routes.ts
@@ -44,11 +44,10 @@ export default [
   {
     path: '/rank',
     icon: 'github',
-    name: '关于网站',
+    name: '支持网站',
     routes: [
       { icon: 'DesktopOutlined', path: '/rank/reward', component: './Rank/Reward', name: '打赏榜 👑', requireAuth: false },
       { icon: 'DesktopOutlined', path: '/rank/about', component: './About', name: '共建与反馈 🚀', requireAuth: false },
-      { icon: 'DesktopOutlined', path: '/rank/other', component: './Other', name: '其他产品 🔥', requireAuth: false },
       {path: 'https://github.com/lhccong/fish-island-backend', name: '狠狠点个 star 🌟'},
 
     ],
